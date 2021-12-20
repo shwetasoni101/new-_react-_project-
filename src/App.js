cdb5dfd12e490a0634a48e2c73d810377fbf7a17
Shweta Soni <shwetasoni709@gmail.com>
2021-12-20 22:16:01 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { createContext, useReducer } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Detail from "./components/Detail";
+import { initialState, reducer } from "./store/reducer";
+
+
+export const AuthContext = createContext();
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        state,
+        dispatch
+      }}
+    >
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login}/>
+        <Route path="/detail" component={Detail}/>
+        <Route path="/" component={Home}/>
+      </Switch>
+    </Router>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;
